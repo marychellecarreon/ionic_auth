7f242d3da3bb9b6e89f617835aa26218b9b259ba
Mary Chelle Carreon <marychellecarreon@gmail.com>
2017-11-09 14:05:31 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { CloudSettings, CloudModule } from '@ionic/cloud/angular';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+const cloudSettings 
 
 @NgModule({
   declarations: [
